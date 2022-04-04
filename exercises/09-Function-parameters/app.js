db4fac5ebcc636c0ad03cc5f2f60ed50db527399
benjalonso <alonsobenjaminlb@gmail.com>
2022-04-04 16:00:34 +0000

    Ejercicios ralizados en la mañana del día 5AM

--- exercises/09-Function-parameters/app.js
+++ exercises/09-Function-parameters/app.js
@@ -1,5 +1,7 @@
 // Your code goes here:
-
+function renderPerson (Nombre, Nacimiento, Color, Edad, Genero){
+  return Nombre + " is a " + Edad + " years old " + Genero + " born in "+ Nacimiento + " with " + Color + " eyes"
+}
 
 
   ///*** Do not edit below this line ****/
